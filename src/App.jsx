658e105dc97110c6b47fa9751763041bc57d6285
Rumaisa10 <sultanarumaisa12@gmail.com>
2025-05-19 16:06:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <h1 className="text-white text-5xl font-extrabold">
+        Tailwind CSS v4.1 + Vite
+      </h1>
+    </div>
+  );
+}
+
+export default App;
